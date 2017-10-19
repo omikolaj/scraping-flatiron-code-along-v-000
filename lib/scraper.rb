1a9b138a8545cd7971231b22d2d0c2ce50bfe522
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-19 10:21:34 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,6 +22,14 @@ class Scraper
     self.get_page.css(".post")
   end
 
+  def make_coures
+    doc.css(".post").each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
 
 
   def print_courses
